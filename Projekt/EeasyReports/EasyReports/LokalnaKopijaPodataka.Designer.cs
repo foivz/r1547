3d75543437e9ff290c38e1202be3db0070a302e1
ezuvic <ezuvic@foi.hr>
2017-02-16 12:52:22 +0100

    Dodan upit na bazu
    Upit vraća time sheet za proslijeđeni projekt i osobu.

--- Projekt/EeasyReports/EasyReports/LokalnaKopijaPodataka.Designer.cs
+++ Projekt/EeasyReports/EasyReports/LokalnaKopijaPodataka.Designer.cs
@@ -4683,12 +4683,19 @@ SELECT id, sifra_projekta, mat_br, opis, datum_i_vrijeme_od, datum_i_vrijeme_do 
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT id, sifra_projekta, mat_br, opis, datum_i_vrijeme_od, datum_i_vrijeme_do F" +
                 "ROM dbo.TimeSheet";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "SELECT id, sifra_projekta, mat_br, opis, datum_i_vrijeme_od, datum_i_vrijeme_do F" +
+                "ROM dbo.TimeSheet\r\nWHERE sifra_projekta = @sifra_projekta AND mat_br = @mat_br";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@sifra_projekta", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "sifra_projekta", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@mat_br", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "mat_br", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -4710,6 +4717,34 @@ SELECT id, sifra_projekta, mat_br, opis, datum_i_vrijeme_od, datum_i_vrijeme_do 
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, true)]
         public virtual LokalnaKopijaPodataka.TimeSheetDataTable GetData() {
             this.Adapter.SelectCommand = this.CommandCollection[0];
+            LokalnaKopijaPodataka.TimeSheetDataTable dataTable = new LokalnaKopijaPodataka.TimeSheetDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillByProjMatBr(LokalnaKopijaPodataka.TimeSheetDataTable dataTable, int sifra_projekta, int mat_br) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            this.Adapter.SelectCommand.Parameters[0].Value = ((int)(sifra_projekta));
+            this.Adapter.SelectCommand.Parameters[1].Value = ((int)(mat_br));
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual LokalnaKopijaPodataka.TimeSheetDataTable GetDataBy(int sifra_projekta, int mat_br) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            this.Adapter.SelectCommand.Parameters[0].Value = ((int)(sifra_projekta));
+            this.Adapter.SelectCommand.Parameters[1].Value = ((int)(mat_br));
             LokalnaKopijaPodataka.TimeSheetDataTable dataTable = new LokalnaKopijaPodataka.TimeSheetDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
