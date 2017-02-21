9bf46b5d8e0a7066839908bd34e9775e53f14e38
ezuvic <ezuvic@foi.hr>
2017-02-21 10:49:44 +0100

    Dodan gumb za izlaz
    Dodan gumb za izlaz na formi

--- Projekt/EeasyReports/EasyReports/Form1.Designer.cs
+++ Projekt/EeasyReports/EasyReports/Form1.Designer.cs
@@ -30,9 +30,10 @@
         {
             this.korisnik = new System.Windows.Forms.Label();
             this.lozinka = new System.Windows.Forms.Label();
-            this.textKorisnik = new System.Windows.Forms.TextBox();
-            this.textLozinka = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtKorisnik = new System.Windows.Forms.TextBox();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnIzlaz = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // korisnik
@@ -53,38 +54,48 @@
             this.lozinka.TabIndex = 2;
             this.lozinka.Text = "Lozinka";
             // 
-            // textKorisnik
+            // txtKorisnik
             // 
-            this.textKorisnik.Location = new System.Drawing.Point(58, 60);
-            this.textKorisnik.Name = "textKorisnik";
-            this.textKorisnik.Size = new System.Drawing.Size(100, 20);
-            this.textKorisnik.TabIndex = 3;
+            this.txtKorisnik.Location = new System.Drawing.Point(58, 60);
+            this.txtKorisnik.Name = "txtKorisnik";
+            this.txtKorisnik.Size = new System.Drawing.Size(100, 20);
+            this.txtKorisnik.TabIndex = 3;
             // 
-            // textLozinka
+            // txtLozinka
             // 
-            this.textLozinka.Location = new System.Drawing.Point(208, 60);
-            this.textLozinka.Name = "textLozinka";
-            this.textLozinka.Size = new System.Drawing.Size(100, 20);
-            this.textLozinka.TabIndex = 4;
+            this.txtLozinka.Location = new System.Drawing.Point(208, 60);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(100, 20);
+            this.txtLozinka.TabIndex = 4;
             // 
-            // button1
+            // btnLogin
             // 
-            this.button1.Location = new System.Drawing.Point(108, 144);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 70);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "LogIn";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnLogin.Location = new System.Drawing.Point(58, 140);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(112, 42);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "LogIn";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnIzlaz
+            // 
+            this.btnIzlaz.Location = new System.Drawing.Point(196, 140);
+            this.btnIzlaz.Name = "btnIzlaz";
+            this.btnIzlaz.Size = new System.Drawing.Size(112, 42);
+            this.btnIzlaz.TabIndex = 6;
+            this.btnIzlaz.Text = "Izlaz";
+            this.btnIzlaz.UseVisualStyleBackColor = true;
             // 
             // EasyReports_ulaznaForma
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(411, 281);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textLozinka);
-            this.Controls.Add(this.textKorisnik);
+            this.Controls.Add(this.btnIzlaz);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.txtKorisnik);
             this.Controls.Add(this.lozinka);
             this.Controls.Add(this.korisnik);
             this.Name = "EasyReports_ulaznaForma";
@@ -100,9 +111,10 @@
 
         private System.Windows.Forms.Label korisnik;
         private System.Windows.Forms.Label lozinka;
-        private System.Windows.Forms.TextBox textKorisnik;
-        private System.Windows.Forms.TextBox textLozinka;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtKorisnik;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnIzlaz;
     }
 }
 
